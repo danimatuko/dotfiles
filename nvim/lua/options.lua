a2ec894cf7ea15013bd4dd600677911a70f37ed9
Dani Matuko <danimatuko@pop-os.lan>
2024-08-27 23:05:02 +0300

    disable listchars

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -56,7 +56,7 @@ vim.opt.smartindent = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+-- vim.opt.list = true
 -- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
