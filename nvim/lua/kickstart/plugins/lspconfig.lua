e35266579690aae85c3debc65e35871f40855473
Dani Matuko <danimatuko@pop-os.lan>
2024-10-14 10:36:36 +0300

    fix LSP for php and WordPress

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
--- LSP Plugins
+-- LSP Pluginslsp
 return {
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -55,10 +55,10 @@ return {
 			-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
 			-- and elegantly composed help section, `:help lsp-vs-treesitter`
 
-			-- This function gets run when an LSP attaches to a particular buffer.
-			-- That is to say, every time a new file is opened that is associated with
-			-- an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-			-- function will be executed to configure the current buffer
+			--  This function gets run when an LSP attaches to a particular buffer.
+			--    That is to say, every time a new file is opened that is associated with
+			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+			--    function will be executed to configure the current buffer
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
@@ -73,28 +73,28 @@ return {
 					end
 
 					-- Jump to the definition of the word under your cursor.
-					-- This is where a variable was first declared, or where a function is defined, etc.
-					-- To jump back, press <C-t>.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 					-- Jump to the implementation of the word under your cursor.
-					-- Useful when your language has ways of declaring types without an actual implementation.
+					--  Useful when your language has ways of declaring types without an actual implementation.
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the type of the word under your cursor.
-					-- Useful when you're not sure what type a variable is and you want to see
-					-- the definition of its *type*, not where it was *defined*.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 					-- Fuzzy find all the symbols in your current document.
-					-- Symbols are things like variables, functions, types, etc.
+					--  Symbols are things like variables, functions, types, etc.
 					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
-					-- Similar to document symbols, except searches over your entire project.
+					--  Similar to document symbols, except searches over your entire project.
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
@@ -102,7 +102,7 @@ return {
 					)
 
 					-- Rename the variable under your cursor.
-					-- Most Language Servers support renaming across files, etc.
+					--  Most Language Servers support renaming across files, etc.
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
@@ -110,12 +110,12 @@ return {
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					-- For example, in C this would take you to the header.
+					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
-					-- See `:help CursorHold` for information about when this is executed
+					--    See `:help CursorHold` for information about when this is executed
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
@@ -168,66 +168,26 @@ return {
 			--  Add any additional override configuration in the following tables. Available keys are:
 			--  - cmd (table): Override the default command used to start the server
 			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Note that default capabilities are automatically included.
+			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+			--  - settings (table): Override the default settings passed when initializing the server.
+			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				intelephense = {
-					settings = {
-						intelephense = {
-							stubs = {
-								"apache",
-								"bcmath",
-								"bz2",
-								"calendar",
-								"Core",
-								"curl",
-								"date",
-								"dom",
-								"filter",
-								"ftp",
-								"gd",
-								"gettext",
-								"hash",
-								"iconv",
-								"imap",
-								"intl",
-								"json",
-								"ldap",
-								"libxml",
-								"mbstring",
-								"mcrypt",
-								"mysql",
-								"mysqli",
-								"password",
-								"pcntl",
-								"pcre",
-								"PDO",
-								"pdo_mysql",
-								"Phar",
-								"posix",
-								"readline",
-								"regex",
-								"session",
-								"SimpleXML",
-								"sockets",
-								"sodium",
-								"standard",
-								"tokenizer",
-								"xml",
-								"xdebug",
-								"xmlreader",
-								"xmlwriter",
-								"yaml",
-								"zip",
-								"zlib",
-							},
-							files = {
-								maxSize = 5000000,
-							},
-						},
-					},
-				},
-				-- Add any other LSP configurations here
+				-- clangd = {},
+				-- gopls = {},
+				-- pyright = {},
+				-- rust_analyzer = {},
+				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				--
+				-- Some languages (like typescript) have entire language plugins that can be useful:
+				--    https://github.com/pmizio/typescript-tools.nvim
+				--
+				-- But for many setups, the LSP (`ts_ls`) will work just fine
+				--
+
 				lua_ls = {
+					-- cmd = {...},
+					-- filetypes = { ...},
+					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
@@ -238,17 +198,114 @@ return {
 						},
 					},
 				},
+
+				-- Add intelephense for PHP LSP support
+				intelephense = {
+					root_dir = function(fname)
+						return vim.fn.getcwd() -- Set the root to the current working directory
+					end,
+					settings = {
+						intelephense = {
+							associations = { "*.php", "*.phtml" }, -- Associate PHP and PHTML files
+							diagnostics = {
+								enable = true, -- Enable diagnostics (linting)
+							},
+							format = {
+								enable = true, -- Enable code formatting
+							},
+							stubs = {
+								"apache",
+								"bcmath",
+								"bz2",
+								"calendar",
+								"com_dotnet",
+								"Core",
+								"ctype",
+								"curl",
+								"date",
+								"dba",
+								"dom",
+								"enchant",
+								"exif",
+								"FFI",
+								"fileinfo",
+								"filter",
+								"fpm",
+								"ftp",
+								"gd",
+								"gettext",
+								"gmp",
+								"hash",
+								"iconv",
+								"imap",
+								"intl",
+								"json",
+								"ldap",
+								"libxml",
+								"mbstring",
+								"meta",
+								"mysqli",
+								"oci8",
+								"odbc",
+								"openssl",
+								"pcntl",
+								"pcre",
+								"PDO",
+								"pdo_ibm",
+								"pdo_mysql",
+								"pdo_pgsql",
+								"pdo_sqlite",
+								"pgsql",
+								"Phar",
+								"posix",
+								"pspell",
+								"readline",
+								"Reflection",
+								"session",
+								"shmop",
+								"SimpleXML",
+								"snmp",
+								"soap",
+								"sockets",
+								"sodium",
+								"SPL",
+								"sqlite3",
+								"standard",
+								"superglobals",
+								"sysvmsg",
+								"sysvsem",
+								"sysvshm",
+								"tidy",
+								"tokenizer",
+								"xml",
+								"xmlreader",
+								"xmlrpc",
+								"xmlwriter",
+								"xsl",
+								"Zend OPcache",
+								"zip",
+								"zlib",
+								"wordpress",
+							},
+						},
+					},
+				},
 			}
 
 			-- Ensure the servers and tools above are installed
+			--  To check the current status of installed tools and/or manually install
+			--  other tools, you can run
+			--    :Mason
+			--
+			--  You can press `g?` for help in this menu.
 			require("mason").setup()
 
-			-- Add PHP formatter to the list of tools to be installed
+			-- You can add other tools here that you want Mason to install
+			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"php-cs-fixer", -- PHP formatter
-				"intelephense", -- PHP language server
 				"stylua", -- Used to format Lua code
+				"intelephense", -- Add PHP LSP to the list of tools to ensure installed
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -258,7 +315,7 @@ return {
 						local server = servers[server_name] or {}
 						-- This handles overriding only values explicitly passed
 						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for tsserver)
+						-- certain features of an LSP (for example, turning off formatting for ts_ls)
 						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 						require("lspconfig")[server_name].setup(server)
 					end,
