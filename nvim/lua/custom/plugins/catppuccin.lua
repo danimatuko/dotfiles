6b3743442cc4cf2ed22482bddd60133f2a33739e
Dani Matuko <danimatuko@pop-os.lan>
2024-10-23 03:03:44 +0300

    disable tokyo-night theme and enable catppuccin

--- nvim/lua/custom/plugins/catppuccin.lua
+++ nvim/lua/custom/plugins/catppuccin.lua
@@ -1,1 +1,12 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
