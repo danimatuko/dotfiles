b83a9ac25fe00e30e8f5c9421ee3c86a13153f2d
danimatuko <danimatuko@outlook.com>
2024-12-10 13:07:43 +0200

    restore options settings

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,110 +1,75 @@
-return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		routes = {
-			{
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-					},
-				},
-				view = "mini",
-			},
-		},
-		presets = {
-			bottom_search = true,
-			command_palette = true,
-			long_message_to_split = true,
-		},
-	},
-	keys = {
-		{ "<leader>sn", "", desc = "+noice" },
-		{
-			"<S-Enter>",
-			function()
-				require("noice").redirect(vim.fn.getcmdline())
-			end,
-			mode = "c",
-			desc = "Redirect Cmdline",
-		},
-		{
-			"<leader>snl",
-			function()
-				require("noice").cmd("last")
-			end,
-			desc = "Noice Last Message",
-		},
-		{
-			"<leader>snh",
-			function()
-				require("noice").cmd("history")
-			end,
-			desc = "Noice History",
-		},
-		{
-			"<leader>sna",
-			function()
-				require("noice").cmd("all")
-			end,
-			desc = "Noice All",
-		},
-		{
-			"<leader>snd",
-			function()
-				require("noice").cmd("dismiss")
-			end,
-			desc = "Dismiss All",
-		},
-		{
-			"<leader>snt",
-			function()
-				require("noice").cmd("pick")
-			end,
-			desc = "Noice Picker (Telescope/FzfLua)",
-		},
-		{
-			"<c-f>",
-			function()
-				if not require("noice.lsp").scroll(4) then
-					return "<c-f>"
-				end
-			end,
-			silent = true,
-			expr = true,
-			desc = "Scroll Forward",
-			mode = { "i", "n", "s" },
-		},
-		{
-			"<c-b>",
-			function()
-				if not require("noice.lsp").scroll(-4) then
-					return "<c-b>"
-				end
-			end,
-			silent = true,
-			expr = true,
-			desc = "Scroll Backward",
-			mode = { "i", "n", "s" },
-		},
-	},
-	config = function(_, opts)
-		if vim.o.filetype == "lazy" then
-			vim.cmd([[messages clear]])
-		end
-		require("noice").setup(opts)
-	end,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
-}
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Configure tabs and indentation: use 4 spaces per tab, replace tabs with spaces, and enable smart indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+-- ets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 8
+
+-- vim: ts=2 sts=2 sw=2 et
+
+-- Refactor: Contextual action (this shows a menu of refactoring options available)
+vim.api.nvim_set_keymap("n", "<leader>rc", ":PhpactorContextMenu<CR>", { noremap = true, silent = true })
+-- use forward slash to comment php single line
+vim.api.nvim_command("autocmd FileType php setlocal commentstring=//%s")
