865189828200f880b2fba1918512294a0858aa09
danimatuko <danimatuko@outlook.com>
2025-07-20 01:27:28 +0300

    waybar alignment fixes

---
+++ config/_waybar/scripts/sys_controls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Icons (you can dynamically change them based on state later)
+BT_ICON="󰂯"
+NOTIF_ICON=""
+POWER_ICON="⏻"
+
+echo "{\"icon\": \"$BT_ICON $NOTIF_ICON $POWER_ICON\"}"
+
