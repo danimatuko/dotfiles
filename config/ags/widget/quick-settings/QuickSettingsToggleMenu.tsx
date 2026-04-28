49e391dda35fa4a26a227e241c5041ddcfaafeb3
danimatuko <danimatuko@outlook.com>
2026-04-28 13:34:56 +0300

    feat:remove ags theme menu button

--- config/ags/widget/quick-settings/QuickSettingsToggleMenu.tsx
+++ config/ags/widget/quick-settings/QuickSettingsToggleMenu.tsx
@@ -2,24 +2,10 @@ import BatteryIndicator from "../bar/BatteryIndicator"
 import BluetoothIndicator from "../bar/BluetoothIndicator"
 import SpeakerIndicator from "../bar/SpeakerIndicator"
 import WifiIndicator from "../bar/WifiIndicator"
-import QuickSettingsToggleButton from "./QuickSettingsToggleButton"
-import { isThemeMenuVisible, toggleThemeMenu } from "../../services/theme-menu"
 
 export default function QuickSettingsToggleMenu() {
   return (
     <box class="quick-settings__toggle-menu" spacing={10}>
-      <QuickSettingsToggleButton
-        iconName="preferences-desktop-theme-symbolic"
-        className={isThemeMenuVisible((visible) =>
-          visible
-            ? "quick-settings__toggle-button quick-settings__toggle-button--active quick-settings__theme-menu-toggle"
-            : "quick-settings__toggle-button quick-settings__theme-menu-toggle",
-        )}
-        onClicked={toggleThemeMenu}
-        sensitive
-        iconOnly
-        tooltipText="Theme switcher"
-      />
       <SpeakerIndicator />
       <WifiIndicator />
       <BluetoothIndicator />
