ae547dff07f9ef826496a35633115e2fdc8ecbd3
danimatuko <danimatuko@outlook.com>
2026-04-29 05:05:08 +0300

    style:polish osd surface and levelbar

--- config/ags/widget/Osd.tsx
+++ config/ags/widget/Osd.tsx
@@ -8,6 +8,9 @@ import {
 } from "../services/quick-settings"
 import { getThemeWindowClass } from "../services/theme"
 
+const osdBottomMargin = 44
+const osdTransitionDuration = 170
+
 export default function Osd(gdkmonitor: Gdk.Monitor) {
   const { BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
 
@@ -18,20 +21,32 @@ export default function Osd(gdkmonitor: Gdk.Monitor) {
       visible={isOsdVisible}
       gdkmonitor={gdkmonitor}
       anchor={BOTTOM | LEFT | RIGHT}
+      marginBottom={osdBottomMargin}
       exclusivity={Astal.Exclusivity.IGNORE}
       application={app}
     >
       <box class="osd__container" halign={Gtk.Align.FILL} hexpand>
-        <box class="osd" spacing={8} halign={Gtk.Align.CENTER}>
-          <image class="osd__icon" iconName={getOsdIcon} />
-          <levelbar
-            class="osd__level"
-            value={getOsdValue}
-            minValue={0}
-            maxValue={1}
-            hexpand
-          />
-        </box>
+        <Gtk.Revealer
+          revealChild={isOsdVisible}
+          transitionType={Gtk.RevealerTransitionType.SLIDE_UP}
+          transitionDuration={osdTransitionDuration}
+        >
+          <box class="osd" spacing={10} halign={Gtk.Align.CENTER}>
+            <image class="osd__icon" iconName={getOsdIcon} />
+            <levelbar
+              class="osd__level"
+              value={getOsdValue}
+              minValue={0}
+              maxValue={1}
+              mode={Gtk.LevelBarMode.CONTINUOUS}
+              hexpand
+            />
+            <label
+              class="osd__value"
+              label={getOsdValue((value) => `${Math.round(value * 100)}%`)}
+            />
+          </box>
+        </Gtk.Revealer>
       </box>
     </window>
   )
