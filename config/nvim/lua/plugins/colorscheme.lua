3b813b9fe29093c88cc73b7a0881e270a85dd4db
danimatuko <danimatuko@outlook.com>
2026-05-13 15:00:15 +0300

    chore:sync active lazyvim config

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "neanias/everforest-nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
