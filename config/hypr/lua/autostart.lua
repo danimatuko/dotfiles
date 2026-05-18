87e85ea100f8c3956c2e4d2e6a705d99a1cd2adf
danimatuko <danimatuko@outlook.com>
2026-05-18 14:22:35 +0300

    fix: adjust hypr autostart order and reload plugins

--- config/hypr/lua/autostart.lua
+++ config/hypr/lua/autostart.lua
@@ -8,12 +8,13 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
+	hl.exec_cmd("ags run ~/.config/ags/app.ts")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("awww-daemon")
 	hl.exec_cmd("hyprsunset")
-	hl.exec_cmd("canberra-gtk-play -i desktop-login")
 	hl.exec_cmd("cliphist daemon")
 	hl.exec_cmd("wl-paste --watch cliphist store")
-	hl.exec_cmd("ags run ~/.config/ags/app.ts")
 	hl.exec_cmd("udiskie")
+	hl.exec_cmd("hyprpm reload")
+	hl.exec_cmd("canberra-gtk-play -i desktop-login")
 end)
