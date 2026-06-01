f4bc7645ace5a721f0c019cdde1c03160195092a
danimatuko <danimatuko@outlook.com>
2026-06-01 15:58:57 +0300

    feat:add catppuccin latte light theme

---
+++ themes/catppuccin-latte/hyprland.lua
@@ -0,0 +1,11 @@
+hl.config({
+  general = {
+    col = {
+      active_border = {
+        colors = { "rgb(7287fd)", "rgb(8839ef)" },
+        angle = 45,
+      },
+      inactive_border = "rgb(bcc0cc)",
+    },
+  },
+})
