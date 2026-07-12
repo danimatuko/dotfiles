35d25f627f76da0e8024a2c34a3cbbab6550aa37
danimatuko <danimatuko@outlook.com>
2026-07-13 00:32:14 +0300

    chore: remove fish config

--- config/fish/conf.d/abbreviations.fish
+++
@@ -1,4 +0,0 @@
-abbr -a -- c clear
-abbr -a -- rm 'rm -i'
-abbr -a -- nv nvim
-abbr -a -- dots 'cd ~/dotfiles && nvim'
--- config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias oc='opencode'
-    # if not set -q ZELLIJ
-    #     zellij -l welcome
-    # end
-end
